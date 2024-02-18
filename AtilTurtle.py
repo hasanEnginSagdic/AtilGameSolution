524c1c8cae9c61698ce8ae1b95a8f82cdcc44601
Hasan Engin SAĞDIÇ <hasanenginsagdic@gmail.com>
2024-02-18 23:19:37 +0300

    game written :)

--- AtilTurtle.py
+++ AtilTurtle.py
@@ -4,10 +4,12 @@ screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Catching Turtle Game")
 FONT = ("Arial", 30, "normal")
-
+scoreGame = 0
+scoreTurtle = turtle.Turtle()
+counterTurtle = turtle.Turtle()
+gameOver = False
 def scoreTurtleSetup():
     #scoreTurtle setup function
-    scoreTurtle = turtle.Turtle()
     scoreTurtle.hideturtle()
     scoreTurtle.penup()
     topHeight = screen.window_height() / 2
@@ -24,6 +26,14 @@ def makeTurtle():
     x = random.randint(leftPoint * 0.75, rightPoint * 0.75)
     y = random.randint(belowPoint * 0.75, topPoint * 0.75)
     t = turtle.Turtle()
+    def handleClick(e, b):
+        #burada turtle'a tıklayınca bir işleme sokulabilir
+        #print(e, b)
+        global scoreGame
+        scoreGame += 1
+        scoreTurtle.clear()
+        scoreTurtle.write(arg=f"Score: {scoreGame}" , move=False, align="center", font=FONT)
+    t.onclick(handleClick)
     t.penup()
     t.shape("turtle")
     t.shapesize(2, 2)
@@ -39,13 +49,41 @@ def hide_turtle():
     for t in turtleList:
         t.hideturtle()
 
+#aşağıdaki koda gerek kalmadı çünkü random.choice diye bir şey varmış
+#turtleNumber = len(turtleList)
+def showTurtleRandomly():
+    if gameOver == True:
+        hide_turtle()
+        random.choice(turtleList).showturtle()
+        screen.ontimer(showTurtleRandomly, 750)
+
+def counter(time=30):
+    global gameOver
+    counterTurtle.hideturtle()
+    counterTurtle.penup()
+    topHeight = screen.window_height() / 2
+    counterTurtle.setpos(0,topHeight * 0.8 - 40)
+    counterTurtle.color("dark blue")
+    counterTurtle.clear()
+    if time > 0:
+        counterTurtle.clear()
+        counterTurtle.write(arg=f"Time: {time}", move=False, align="center", font=FONT)
+        #lambda vermem lazım yeni fonsiyon gibi düşünüyor
+        screen.ontimer(lambda: counter(time - 1), 1000)
+    else:
+        gameOver = True
+        counterTurtle.clear()
+        hide_turtle()
+        counterTurtle.write(arg="game over!!!", move=False, align="center", font=FONT)
+
 
 turtle.tracer(0)
 
 setupTurtles()
 scoreTurtleSetup()
+counter(10)
 hide_turtle()
-
+showTurtleRandomly()
 
 turtle.tracer(1)
 
